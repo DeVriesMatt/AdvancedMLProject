933862d9d35bcd77880f7933bbfacf227903ad98
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-17 23:59:58 +0100

    Cleaned adasyn

--- src/adasyn.py
+++ src/adasyn.py
@@ -1,4 +1,3 @@
-from keras import backend
 from imblearn.over_sampling import SMOTE, ADASYN
 import os
 import pandas as pd
@@ -8,27 +7,28 @@ import matplotlib.pyplot as plt
 heart_path = "./datasets/heartbeat/"
 matlab_path = "./datasets/datasets_without_augmentation/matlab/train/"
 oversampled = "./datasets/oversampled/"
+
 # List the datasets contained in heartbeats
 print(os.listdir(heart_path))
 
-# # Import the train and test datsets
-# train = pd.read_csv(matlab_path + "mitbih_train_without4.csv", header=None)
-# train = train.iloc[1:]
-#
-# # Print the number of observations of each class
-# train[187] = train[187].astype(int)
-# equilibre = train[187].value_counts()
-# print("The value counts for the training set before SMOTE:")
-# print(equilibre)
-#
-# plt.figure(figsize=(20, 10))
-# my_circle = plt.Circle((0, 0), 0.7, color='white')
-# plt.pie(equilibre, labels=['N', 'V', 'S', 'F'],
-#         colors=['red', 'green', 'blue', 'skyblue'],
-#         autopct='%1.1f%%')
-# p = plt.gcf()
-# p.gca().add_artist(my_circle)
-# plt.show()
+# Import the train and test datsets
+train = pd.read_csv(matlab_path + "mitbih_train_without4.csv", header=None)
+train = train.iloc[1:]
+
+# Print the number of observations of each class
+train[187] = train[187].astype(int)
+equilibre = train[187].value_counts()
+print("The value counts for the training set before SMOTE:")
+print(equilibre)
+
+plt.figure(figsize=(20, 10))
+my_circle = plt.Circle((0, 0), 0.7, color='white')
+plt.pie(equilibre, labels=['N', 'V', 'S', 'F'],
+        colors=['red', 'green', 'blue', 'skyblue'],
+        autopct='%1.1f%%')
+p = plt.gcf()
+p.gca().add_artist(my_circle)
+plt.show()
 
 # Oversample the training sets in matlab folder
 for filename in os.listdir(matlab_path):
@@ -59,7 +59,7 @@ for filename in os.listdir(matlab_path):
         y_res_pd = pd.DataFrame(y_res)
         print("The value counts for the training set after SMOTE:")
         print(y_res_pd.sum(axis=0))
-        y_res_pd.to_csv(oversampled + filename + "_smote.csv")
+        y_res_pd.to_csv(oversampled + "smote_" + filename)
 
         # Perform Adasyn
         ada = ADASYN(random_state=42)
@@ -68,36 +68,5 @@ for filename in os.listdir(matlab_path):
         y_res_pd_ada = pd.DataFrame(y_res_ada)
         print("The value counts for the whole dataset after ADASYN:")
         print(y_res_pd_ada.sum(axis=0))
-        y_res_pd_ada.to_csv(oversampled + filename + "_adasyn.csv")
+        y_res_pd_ada.to_csv(oversampled + "adasyn_" + filename)
 
-
-# # Setting up the variables
-# target_train = train[187]
-# target_test = test[187]
-# y_train = to_categorical(target_train)
-# y_test = to_categorical(target_test)
-# print("Y train shape: {}".format(y_train.shape))
-#
-# X_train = train.iloc[:, :186].values
-# X_test = test.iloc[:, :186].values
-# print("X train shape: {}".format(X_train.shape))
-#
-# # Perform SMOTE firstly on only the raw training set
-# sm = SMOTE(random_state=42)
-# X_res, y_res = sm.fit_resample(X_train, y_train)
-#
-# y_res_pd = pd.DataFrame(y_res)
-# print("The value counts for the training set after SMOTE:")
-# print(y_res_pd.sum(axis=0))
-# y_res_pd.to_csv(oversampled + "kaggle_smote_train.csv")
-#
-# # Perform Adasyn firstly on only the raw training set
-#
-# ada = ADASYN(random_state=42)
-# X_res_ada, y_res_ada = ada.fit_resample(X_train, y_train)
-#
-# y_res_pd_ada = pd.DataFrame(y_res_ada)
-# print("The value counts for the whole dataset after ADASYN:")
-# print(y_res_pd_ada.sum(axis=0))
-# y_res_pd_ada.to_csv(oversampled + "kaggle_adasyn_train.csv")
-#
