7e7806f8f6c7cf6d3669cbffe56d972d5b275608
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-20 13:15:11 +0100

    Update results

--- src/classifiers_aug.py
+++ src/classifiers_aug.py
@@ -10,7 +10,7 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 names = ["NearestNeighbors",
-         "LinearSVM",
+         # "LinearSVM",
          "DecisionTree",
          "RandomForest",
          "NeuralNet",
@@ -19,7 +19,7 @@ names = ["NearestNeighbors",
 
 classifiers = [
     KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025, verbose=True),
+    # SVC(kernel="linear", C=0.025, verbose=True),
     # SVC(gamma=2, C=1),
     # GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
@@ -47,31 +47,31 @@ print(train_dir)
 print(test_dir)
 
 
-for dataset in range(3, len(test_dir)):
-
-    train = pd.read_csv(oversampled + train_dir[dataset], header=None)
-    test = pd.read_csv(matlab_test + test_dir[dataset], header=None)
-
-    train = train.iloc[1:, 1:].astype(float)
-    test = test.iloc[1:].astype(float)
-
-    X_train = train.iloc[:, 0:(train.shape[1]-1)]
-    y_train = train.iloc[:, train.shape[1]-1]
-
-    X_test = test.iloc[:, 0:(test.shape[1]-1)]
-    y_test = test.iloc[:, test.shape[1]-1]
-
-    print('\n--- Running classifiers for ---' + train_dir[dataset])
-    for name, clf in zip(names, classifiers):
-        print(name)
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        class_report = classification_report(y_test, y_pred, output_dict=True)
-        conf_mat_test = confusion_matrix(y_test, y_pred)
-        report = pd.DataFrame(class_report).transpose()
-        report.to_csv(results + 'report_' + name + "_" + train_dir[dataset])
-        cmat = pd.DataFrame(conf_mat_test)
-        cmat.to_csv(results + 'confus_' + name + "_" + train_dir[dataset])
+# for dataset in range(3, len(test_dir)):
+#
+#     train = pd.read_csv(oversampled + train_dir[dataset], header=None)
+#     test = pd.read_csv(matlab_test + test_dir[dataset], header=None)
+#
+#     train = train.iloc[1:, 1:].astype(float)
+#     test = test.iloc[1:].astype(float)
+#
+#     X_train = train.iloc[:, 0:(train.shape[1]-1)]
+#     y_train = train.iloc[:, train.shape[1]-1]
+#
+#     X_test = test.iloc[:, 0:(test.shape[1]-1)]
+#     y_test = test.iloc[:, test.shape[1]-1]
+#
+#     print('\n--- Running classifiers for ---' + train_dir[dataset])
+#     for name, clf in zip(names, classifiers):
+#         print(name)
+#         clf.fit(X_train, y_train)
+#         y_pred = clf.predict(X_test)
+#         class_report = classification_report(y_test, y_pred, output_dict=True)
+#         conf_mat_test = confusion_matrix(y_test, y_pred)
+#         report = pd.DataFrame(class_report).transpose()
+#         report.to_csv(results + 'report_' + name + "_" + train_dir[dataset])
+#         cmat = pd.DataFrame(conf_mat_test)
+#         cmat.to_csv(results + 'confus_' + name + "_" + train_dir[dataset])
 
 
 for dataset in range(len(test_dir), len(train_dir)):
